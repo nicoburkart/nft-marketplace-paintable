3f9bc400616a8ffaf9f4cf48be4efed9f2fea4d7
Nico Burkart <nico@MacBook-Air-von-Nico.local>
2021-10-28 11:17:50 +0100

    basic architecture

---
+++ src/modules/home/components/hero/hero.tsx
@@ -0,0 +1,11 @@
+import './hero.scss';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/modules/home/home.tsx
@@ -0,0 +1,11 @@
+import Hero from './components/hero/hero';
+
+function Home() {
+  return (
+    <div className="home">
+      <Hero></Hero>
+    </div>
+  );
+}
+
+export default Home;
